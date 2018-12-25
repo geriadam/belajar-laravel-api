b07e1bc6559cfee11b825b3bc904367098d02866
DESKTOP-UPQ61CI\geria <geriadam1@gmail.com>
2018-12-25 13:25:36 +0700

    change format api

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,80 +9,16 @@ class ProductController extends Controller
 {
     public function index()
     {
-    	$result   = [];
-    	$products = Product::all();
+    	$products = Product::paginate(5);
 
-
-    	if($products){
-    		foreach ($products as $i => $product) {
-    			$data[$i] = [
-					"name"        => $product->name,
-					"description" => $product->description,
-					"price"       => $product->price,
-					"stock"       => $product->stock == 0 ? "Out of Stock" : $product->stock,
-					"discount"    => $product->discount,
-					"totalPrice"  => round(( 1 - ($product->discount / 100)) * $product->price, 2),
-					"rating"      => $product->reviews->sum('star') == 0 ? 'No rating yet' : round($product->reviews->sum('star') / $product->reviews->count(), 2),
-    				"href" 		  => [
-    									'reviews' => route('reviews.index', $product->id)
-    								]
-    			];
-    		}
-    	}
-
-    	if($data){
-    		$result = [
-    			"message" => "Data found",
-    			"status"  => 200,
-    			"data" 	  => $data, 
-    		];
-    	} else {
-    		$result = [
-    			"message" => "Data not found",
-    			"status"  => 403,
-    			"data" 	  => [], 
-    		];
-    	}
-
-    	return $result;
+    	return $products;
 
     }
 
     public function show(Product $product)
     {
-    	$result  = [];
     	$product = Product::find($product->id);
 
-
-    	if($product){
-    		$data[0] = [
-				"name"        => $product->name,
-				"description" => $product->description,
-				"price"       => $product->price,
-				"stock"       => $product->stock == 0 ? "Out of Stock" : $product->stock,
-				"discount"    => $product->discount,
-				"totalPrice"  => round(( 1 - ($product->discount / 100)) * $product->price, 2),
-				"rating"      => $product->reviews->sum('star') == 0 ? 'No rating yet' : round($product->reviews->sum('star') / $product->reviews->count(), 2),
-				"href" 		  => [
-									'reviews' => route('reviews.index', $product->id)
-								]
-			];
-    	}
-
-    	if($data){
-    		$result = [
-    			"message" => "Data found",
-    			"status"  => 200,
-    			"data" 	  => $data, 
-    		];
-    	} else {
-    		$result = [
-    			"message" => "Data not found",
-    			"status"  => 403,
-    			"data" 	  => [], 
-    		];
-    	}
-
-    	return $result;
+    	return $product;
     }
 }
--- app/Http/Controllers/ReviewsController.php
+++ app/Http/Controllers/ReviewsController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Model\Review;
+use App\Model\Product;
 
 class ReviewsController extends Controller
 {
-    //
+    public function index(Product $product)
+    {
+    	$reviews  = $product->reviews()->paginate(5);
+
+    	return $reviews;
+
+    }
+
+    public function show(Product $product, Review $review)
+    {
+    	$review  = $product->reviews()->find($review->id);
+
+    	return $review;
+    }
 }
